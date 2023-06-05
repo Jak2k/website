5a81c72d8834322a4506590b15b7e6578779fd3a
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-06-05 17:01:06 +0000

    ci: :rocket: add vercel-adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jak2k.schwanenberg.name",
   output: "server",
   integrations: [mdx(), sitemap()],
+  adapter: vercel()
 });
