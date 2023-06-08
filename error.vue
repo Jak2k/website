792c8dd91ec17c027ad3cea91b9838abda3cca1d
jak2k <76660118+Jak2k@users.noreply.github.com>
2023-06-08 19:31:18 +0200

    feat: :sparkles: enable routing

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const route = useRoute();
+defineProps(["error"]);
+</script>
+
+<template :layout="error">
+  <div>
+    <h1 v-if="error.statusCode === 404">404 - Page Not Found</h1>
+    <h1 v-else>Error {{ error.statusCode }}</h1>
+    <p>
+      Are you sure
+      <code>
+        <pre>{{ route.fullPath }}</pre>
+      </code>
+      exists?
+    </p>
+  </div>
+</template>
