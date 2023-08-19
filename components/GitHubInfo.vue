43379597a4609da64daf0770cd908a5fbac69004
jak2k <76660118+Jak2k@users.noreply.github.com>
2023-08-19 21:39:49 +0200

    fix: :bug: fix github-info

--- components/GitHubInfo.vue
+++ components/GitHubInfo.vue
@@ -1,65 +1,64 @@
-<template>
-  <div>
-    <!-- Display the last active day on GitHub -->
-    <p>Last active day: {{ lastActiveDay }}</p>
+<script lang="ts" setup>
+const lastActiveDay = ref("");
+const totalStars = ref(0);
 
-    <!-- Display the total stars on GitHub -->
-    <p>Total stars: {{ totalStars }}</p>
-
-    <!-- Add a "visit me on GitHub" button -->
-    <a href="https://github.com/Jak2k" target="_blank">Visit me on GitHub</a>
-  </div>
-</template>
-
-<script lang="ts">
-  function timeAgoFromLatestEvent(events: any[]): string {
+function timeAgoFromLatestEvent(events: any[]): string {
   if (events.length === 0) {
     return "No events found.";
   }
 
   // Find the latest event based on the "created_at" property
   const latestEvent = events.reduce((prev, current) =>
-    new Date(current.created_at).getTime() > new Date(prev.created_at).getTime() ? current : prev
+    new Date(current.created_at).getTime() > new Date(prev.created_at).getTime()
+      ? current
+      : prev
   );
   const latestEventTime = new Date(latestEvent.created_at);
   const currentTime = new Date();
 
-  const timeDifferenceInSeconds = Math.floor((currentTime.getTime() - latestEventTime.getTime()) / 1000);
+  const timeDifferenceInSeconds = Math.floor(
+    (currentTime.getTime() - latestEventTime.getTime()) / 1000
+  );
   const timeDifferenceInMinutes = Math.floor(timeDifferenceInSeconds / 60);
 
-  const formatter = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
-  const timeAgo = formatter.format(-timeDifferenceInMinutes, 'minute');
+  const formatter = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+  const timeAgo = formatter.format(-timeDifferenceInMinutes, "minute");
 
-  return `${timeAgo} (${latestEvent.type}`;
+  return `${timeAgo} (${latestEvent.repo.name})`;
 }
 
+onMounted(() => {
+  fetchGitHubInfo();
+});
 
-export default {
-  data() {
-    return {
-      lastActiveDay: '',
-      totalStars: 0
-    };
-  },
-  mounted() {
-    this.fetchGitHubInfo();
-  },
-  methods: {
-    async fetchGitHubInfo() {
-      try {
-        // Fetch the last active day from the GitHub API
-        const lastActiveResponse = await fetch('https://api.github.com/users/Jak2k/events');
-        const lastActiveData = await lastActiveResponse.json();
-        this.lastActiveDay = timeAgoFromLatestEvent(lastActiveData);
+async function fetchGitHubInfo() {
+  try {
+    // Fetch the last active day from the GitHub API
+    const lastActiveResponse = await fetch(
+      "https://api.github.com/users/Jak2k/events"
+    );
+    const lastActiveData = await lastActiveResponse.json();
+    lastActiveDay.value = timeAgoFromLatestEvent(lastActiveData);
 
-        // Fetch the total stars from the GitHub API
-        const starsResponse = await fetch('https://api.github.com/users/Jak2k/repos');
-        const starsData = await starsResponse.json();
-        this.totalStars = starsData.reduce((total, repo) => total + repo.stargazers_count, 0);
-      } catch (error) {
-        console.error('Error fetching GitHub info:', error);
-      }
-    }
+    // Fetch the total stars from the GitHub API
+    const starsResponse = await fetch(
+      "https://api.github.com/users/Jak2k/repos"
+    );
+    const starsData = await starsResponse.json();
+    totalStars.value = starsData.reduce(
+      (total: number, repo: { stargazers_count: number }) =>
+        total + repo.stargazers_count,
+      0
+    );
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Error fetching GitHub info:", error);
   }
-};
+}
 </script>
+<template>
+  <div>
+    <p>Last active day: {{ lastActiveDay }}</p>
+    <p>Total stars: {{ totalStars }}</p>
+  </div>
+</template>
