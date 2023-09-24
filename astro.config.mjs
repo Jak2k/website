5981b93ebfe14df3bab8d05399d3f7d93cb51ebe
jak2k <76660118+Jak2k@users.noreply.github.com>
2023-09-24 19:23:14 +0200

    feat: :sparkles: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // astro.config.ts
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import sitemap from "astro-sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true, // or a path to the reset file
     }),
+    sitemap(),
   ],
   server: {
     port: 3000,
   },
+  site: "https://jak2k.schwaneberg.name",
 });
