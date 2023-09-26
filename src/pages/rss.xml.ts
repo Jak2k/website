a49589d194af3fed4c0d881191b93f99bba697c1
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-09-26 12:19:05 +0000

    fix: :bug: add missing description in rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,19 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context: any) {
-  const blog = await getCollection('posts');
+  const blog = await getCollection("posts");
   return rss({
     title: `Jak2k' Blog`,
-    description: 'A blog about web development, linux and other stuff',
+    description: "A blog about web development, linux and other stuff",
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.lastUpdated,
-      description: post.body.split('\n')[0],
+      description:
+        (post.body.split("\n")[0] ||
+          post.body.split("\n")[1] ||
+          "Unknown description") + "\nRead more...",
       link: `/post/${post.slug}/`,
     })),
   });
