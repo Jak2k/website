5fd201941eed456776d7bcbe45e8df67035e5ad1
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-10-26 18:12:21 +0200

    5 why i switched from react to vue (#31)
    * fix: :bug: fix tag filtering and analytics bug
    * finish post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,18 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import sitemap from "astro-sitemap";
 import vercel from "@astrojs/vercel/static";
+import vue from "@astrojs/vue";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true, // or a path to the reset file
-    }),
-    sitemap(),
-  ],
+  integrations: [UnoCSS({
+    injectReset: true // or a path to the reset file
+  }), sitemap(), vue(), mdx()],
   server: {
-    port: 3000,
+    port: 3000
   },
   site: "https://jak2k.schwanenberg.name",
-  adapter: vercel(),
+  adapter: vercel()
 });
