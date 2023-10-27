7baa0987b13f0e4141f053c951e51ced48489afe
jak2k <76660118+Jak2k@users.noreply.github.com>
2023-10-27 21:07:53 +0200

    fix(content): :sparkles: enhance `switch-react-vue`

---
+++ src/components/demo/vue/ModelDemo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const name = ref("");
+</script>
+<template>
+  <div>
+    <input v-model="name" />
+    <p>Hello {{ name }}</p>
+  </div>
+</template>
