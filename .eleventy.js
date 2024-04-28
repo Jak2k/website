c9a11a150be288afc98ffe4a1348b2c9db57723f
jak2k <76660118+Jak2k@users.noreply.github.com>
2024-04-28 17:10:53 +0200

    refactor: :fire: remove old source

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("website/styles.css");
   eleventyConfig.addPassthroughCopy("website/webmentions.js")
   eleventyConfig.addPassthroughCopy("website/ab-quiz.js")
+  eleventyConfig.addPassthroughCopy("website/.well-known/**/*")
   eleventyConfig.addCollection("post", function (collection) {
     return collection.getFilteredByGlob("website/post/**/*.md");
   });
--- README.md
+++
@@ -1,7 +0,0 @@
-# Jak2k's Website
-
-## Commands
-
-- `pnpm i` - Install the dependencies
-- `pnpm dev` - Start the development server
-- `pnpm build` - Build the website
